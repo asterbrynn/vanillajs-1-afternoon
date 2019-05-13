baa64c089df54abde40c87a25945f357e9f1a83e
Aster Brynn <aster.gibby@gmail.com>
2019-05-13 13:48:43 -0600

    part 2 black diamond finished

--- Part2/index.js
+++ Part2/index.js
@@ -1,10 +1,18 @@
 console.log("The house always wins!")
 
-const id = document.getElementById("id-input")
-const color = document.getElementById("style-input")
+let id = document.getElementById("id-input")
+let color = document.getElementById("style-input")
 
 function setCard() {
-	let card = id.value;
-	console.log(card);
+	let card = document.getElementById(id.value);
 	card.style.color = color.value;
+}
+
+function resetCards() {
+	let cards = document.getElementsByClassName("suit");
+	for (i = 0; i < cards.length; i++) {
+		if(cards[i].style.color) {
+				cards[i].style.color = "#808080";
+		}
+	}
 }
